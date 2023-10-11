7a161de25f739236594a3af2c8bd48c03c6d6498
nare25753 <charannarendra656@gmail.com>
2023-10-11 16:47:19 +0530

    update code

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 const local="mongodb://127.0.0.1:27017/Patenta"
 const db="mongodb+srv://narendracharan257538:jwvd0y0iMQthnnTL@cluster0.2miiukh.mongodb.net/test"
 mongoose
-  .connect(local)
+  .connect(db)
   .then(() => {
     console.log("Successfully connected to mongoDB");
   })
