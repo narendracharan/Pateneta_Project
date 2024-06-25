a582d018baaa0d1a0aef36eea15ab16fdccab341
nare25753 <charannarendra656@gmail.com>
2024-06-25 18:16:39 +0530

    done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 // app.use(helmet());
-app.use(cors());
+app.use(cors({origin:url}));
 app.use(bodyparser.json());
 app.use(morgan("tiny"));
 require("./config/connection");
