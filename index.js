a523f311c8aa6be69375d5032e69a939a326a100
nare25753 <charannarendra656@gmail.com>
2024-06-25 19:03:09 +0530

    done

--- index.js
+++ index.js
@@ -17,9 +17,24 @@ const io = new Server(server);
 // app.use(helmet());
 app.use(
   cors({
-    origin: "*"
+    origin: "*",
   })
 );
+var allowlist = [
+  "https://patenta-sa.com",
+  "https://admin.patenta-sa.com",
+  "http://localhost:3001",
+  "http://localhost:3000",
+];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 app.use(bodyparser.json());
 app.use(morgan("tiny"));
 require("./config/connection");
