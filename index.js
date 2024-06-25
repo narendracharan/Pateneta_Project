f6a66d663b41b5a8719527e44587892b6751e81e
nare25753 <charannarendra656@gmail.com>
2024-06-25 18:21:55 +0530

    done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 // app.use(helmet());
-app.use(cors({origin:url}));
+app.use(cors({origin:"https://patenta-sa.com"}));
 app.use(bodyparser.json());
 app.use(morgan("tiny"));
 require("./config/connection");
